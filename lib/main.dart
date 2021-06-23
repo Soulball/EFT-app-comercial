86491660ed1f52f7287fb9986576fd99181378df
EsZa11 <76127819+EsZa11@users.noreply.github.com>
2021-06-23 14:10:10 -0700

    delete

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:eft_app_comercial/Libraries/decoration_colors.dart';
 import 'package:eft_app_comercial/Pages/OtherProducts/otherproduct.dart';
-import 'package:eft_app_comercial/Widgets/OtherProducts/prueba.dart';
 import 'package:flutter/material.dart';
 import 'Pages/Login/login.dart';
-import 'Widgets/OtherProducts/datetable.dart';
 
 //Inicio de la aplicacion
 void main() => runApp(MyApp());
@@ -13,8 +11,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(primaryColor: whiteDark),
-        //home: DateT(),
-        //home: MyHomePage(title: 'Flutter Demo Home Page'),
         home: Login(),
         debugShowCheckedModeBanner: false);
   }
