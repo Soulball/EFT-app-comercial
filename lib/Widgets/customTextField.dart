b69806a41594b1be2b3852fbaf1c9d4317cfd8b6
Enrique De Jesus Navarrete <navarrete.de16@tectijuana.edu.mx>
2021-07-07 13:16:39 -0700

    Correciónes TextField

--- lib/Widgets/customTextField.dart
+++ lib/Widgets/customTextField.dart
@@ -4,27 +4,53 @@ import 'package:eft_app_comercial/Widgets/customText.dart';
 import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   //Variables
   int lines;
   String title;
+  final controller = TextEditingController();
 
-  //Constructor
   CustomTextField({this.lines, this.title});
 
-  //Widget
+  void _printLatestValue() {
+    print('Second text field: ${controller.text}');
+  }
+
+  @override
+  _CustomTextFieldState createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  @override
+  void dispose() {
+    widget.controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    widget.controller.addListener(widget._printLatestValue);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
       CustomText(
-          data: title, size: 14, color: grayText, weight: FontWeight.bold),
+          data: widget.title,
+          size: 14,
+          color: grayText,
+          weight: FontWeight.bold),
       Container(
-          height: 20 * lines.toDouble() + 16,
+          height: 20 * widget.lines.toDouble() + 16,
           decoration: BoxDecoration(
               border: Border.all(), borderRadius: BorderRadius.circular(8)),
           padding: EdgeInsets.all(4),
           child: TextField(
-              autofocus: false, maxLines: lines, textAlign: TextAlign.justify))
+              controller: widget.controller,
+              autofocus: false,
+              maxLines: widget.lines,
+              textAlign: TextAlign.justify))
     ]);
   }
 }
